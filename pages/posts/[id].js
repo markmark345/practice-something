60b0aa031c65b01cb82f60de87f20a977647f648
markmark345 <natapatchara.anuroje@gmail.com>
2021-08-14 04:07:00 +0700

    second commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,55 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+      paths,
+      fallback: false
+    }
+    // return [
+    //     {
+    //       params: {
+    //         // Statically Generates /posts/a/b/c
+    //         id: ['pre-rendering', 'ssg-ssr']
+    //       }
+    //     }
+    //     //...
+    //   ]
+  }
+
+  export async function getStaticProps({ params }) {
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id)
+    // ...
+    return {
+        props: {
+          postData
+        }
+      }
+  }
+
+//   export async function getStaticProps({ params }) {
+//     // params.id will be like ['a', 'b', 'c']
+//   }
+
+  export default function Post({ postData }) {
+    return (
+      <Layout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </Layout>
+    )
+  }
+
